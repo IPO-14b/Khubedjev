ebfe5f2a48a113c8a55697f22edb2839002f18d4
Khubedjev <hubedjev.d.p@gmail.com>
2017-05-26 02:14:02 +0300

    Тестовый комит

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 var pjs = new PointJS('2D', 1280 / 2, 720 / 2, { // 16:9
-    backgroundColor : '#53769A' // if need
+  backgroundColor : '#53769A' // if need
 });
 pjs.system.initFullPage(); // for Full Page mode
 
@@ -16,8 +16,11 @@ var key   = pjs.keyControl.initKeyControl();
 
 var width  = game.getWH().w; // width of scene viewport
 var height = game.getWH().h; // height of scene viewport
+// Получим резолюцию экрана
+var r = game.getResolution();
 
-pjs.system.setTitle('PointJS Game'); // Set Title for Tab or Window
+
+pjs.system.setTitle('Happy New Year Game'); // Set Title for Tab or Window
 
 game.newLoopFromConstructor('myGame', function () {
 
@@ -34,15 +37,20 @@ game.newLoopFromConstructor('myGame', function () {
     h : height/1.5 * r // Растягивание фона под экран
   });
 
-  var djostik = game.newImageObject({
-    h : 200 * r, 
+  // Теперь создадим деда мороза (ну или санту)
+  var santa = game.newImageObject({
+    file : 'djostik.png',
+    h : 200 * r, // Оптимальный размер санты
     onload : function () {
       // отпозиционируем его по высоте
       this.y = -this.h + height + 20*r; // Отлично
     }
   });
-  var points = [];
 
+  // Объявим массив с подарками
+  var podarki = [];
+
+  // Создадим таймер, который будет добавлять подарки
   var timer = OOP.newTimer(1000, function () {
     podarki.push(game.newImageObject({
       x : math.random(0, width - 50*r), // 50*r - ширина объекта
@@ -63,6 +71,10 @@ game.newLoopFromConstructor('myGame', function () {
     back.draw(); // Отрисуем фон
     santa.draw(); // Отрисуем санту
 
+    // Алгоритм добавления подарков по таймеру
+    // новый подарок каждую секунду
+
+    // Для того, чтобы подарки добавлялись каждую секунду
     timer.restart();
 
     OOP.forArr(podarki, function (el, i) { // i - идентификатор
@@ -70,6 +82,7 @@ game.newLoopFromConstructor('myGame', function () {
 
       el.move(point(0, speed*dt)); // Двигаем вниз
 
+      // Проверка на столкновение подарка с сантой
 
       if (el.isIntersect(santa)) {
         podarki.splice(i, 1); // i - идентификатор, 1 - количество
@@ -78,6 +91,9 @@ game.newLoopFromConstructor('myGame', function () {
       }
 
     });
+
+    // Заставим двигатьcz санту
+    // Учтем ограничения движения
 
     if (key.isDown('LEFT')) {
       // Двигаем влево
