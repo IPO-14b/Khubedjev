1de864e9f3256a91751a7161e7986aa170da568c
Khubedjev <hubedjev.d.p@gmail.com>
2017-05-26 01:49:32 +0300

    Небольшой фикс

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var pjs = new PointJS('2D', 1280 / 2, 720 / 2, { // 16:9
 	backgroundColor : '#53769A' // if need
 });
+pjs.system.initFullPage(); // for Full Page mode
 
 var log    = pjs.system.log;     // log = console.log;
 var game   = pjs.game;           // Game Manager
@@ -10,6 +11,8 @@ var brush  = pjs.brush;          // Brush, used for simple drawing
 var OOP    = pjs.OOP;            // Object's manager
 var math   = pjs.math;           // More Math-methods
 var levels = pjs.levels;         // Levels manager
+
+var key   = pjs.keyControl.initKeyControl();
 
 var width  = game.getWH().w; // width of scene viewport
 var height = game.getWH().h; // height of scene viewport
