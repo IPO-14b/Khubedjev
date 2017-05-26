e47b27d5b410a93d00d8f18b4030a4102446884c
Khubedjev <hubedjev.d.p@gmail.com>
2017-05-26 19:07:30 +0300

    Исправил ошибки

--- game.js
+++ game.js
@@ -3,7 +3,7 @@
  *
  * Используется для инициальзции всех переменных и объектов движка
  *
- * @var $pjs
+ * @var Object $pjs
  */
 var pjs = new PointJS('2D', 1280 / 2, 720 / 2, { // 16:9
     backgroundColor : '#53769A' // if need
@@ -17,7 +17,7 @@ pjs.system.initFullPage(); // for Full Page mode
  *
  * @var int $width
  */
-var width  = pjs.game.getWH().w; 
+var width = pjs.game.getWH().w; 
 /**
  * высота сцены
  *
@@ -65,7 +65,7 @@ pjs.game.newLoopFromConstructor('myGame', function () {
     *
     * Испоьзуется дляя создания фона игры
     *
-    * @var  $back
+    * @var Object $back
     */    
     var back = pjs.game.newImageObject({
         file : 'background.jpg',
@@ -77,15 +77,11 @@ pjs.game.newLoopFromConstructor('myGame', function () {
     *
     * Испоьзуется дляя создания объекта джостика
     *
-    * @var  $djostik
+    * @var Object $djostik
     */
     var djostik = pjs.game.newImageObject({
         file : 'djostik.png',
-        //<<<<<<< lab4
-        //    h : 100 * r, 
-        //=======
-        h : 100 * r, // Оптимальный размер санты
-        //>>>>>>> master
+        h : 100 * r, 
         /**
         * Функция оптимизации джостика под экран
         *
@@ -97,89 +93,103 @@ pjs.game.newLoopFromConstructor('myGame', function () {
         }
     });
 
-//<<<<<<< lab4
-//=======
-// Объявим массив с подарками
-//>>>>>>> master
-    var pjs.vector.points = [];
+    /**
+    * Массив поинтов
+    *
+    * Используется для хранения и создания поинтов
+    *
+    * @var Object $points
+    */
+    var points = [];
 
+    /**
+    * Объект таймер
+    *
+    * Испоьзуется для создания таймера
+    *
+    * @var Object $timer
+    */
     var timer = pjs.OOP.newTimer(1000, function () {
-        pjs.vector.points.push(pjs.game.newImageObject({
+        points.push(pjs.game.newImageObject({
             x : pjs.math.random(0, width - 50*r), // 50*r - ширина объекта
             y : -pjs.math.random(50*r, 500*r), // уберем минус, так как он уже есть
             w : 20*r, h : 20*r,
-            file : 'pjs.vector.point.png'
+            file : 'point.png'
         }));
     });
-
+    /**
+    * Функция обновления
+    *
+    * Функция, которая отвечает за обновление экрана
+    */
     this.update = function () {
 
-    // Задействуем фактор дельта-тайм
-    var dt = pjs.game.getDT(10); // 10 - это делитель дкльты для
-    // удобного округления
+        /**
+        * Объект дельта-тайм
+        *
+        * Задействуем фактор дельта-тайм
+        *
+        * @var Object $dt
+        */
+        var dt = pjs.game.getDT(10); // 10 - это делитель дкльты для
+        // удобного округления
 
-    pjs.game.clear(); // clear screen
+        pjs.game.clear(); // clear screen
 
-    back.draw(); // Отрисуем фон
-//<<<<<<< lab4
-//    djostik.draw(); 
-//=======
-    djostik.draw(); // Отрисуем санту
-    //>>>>>>> master
+        back.draw(); // Отрисуем фон
+        djostik.draw(); // Отрисуем санту
 
-    timer.restart();
+        timer.restart();
+        /**
+        * Функция усложнения
+        *
+        * Функция, которая отвечает за счет и увелечение скорости
+        */
+        pjs.OOP.forArr(points, function (el, i) { // i - идентификатор
+            el.draw(); 
 
-    pjs.OOP.forArr(pjs.vector.points, function (el, i) { // i - идентификатор
-//<<<<<<< lab4
-//      el.draw(); 
-//
-//      el.move(pjs.vector.point(0, speed*dt)); // Двигаем вниз
-//
-//=======
-        el.draw(); // Рисуем подарок
+            el.move(pjs.vector.point(0, speed*dt)); // Двигаем вниз
 
-        el.move(pjs.vector.point(0, speed*dt)); // Двигаем вниз
+            if (el.isIntersect(djostik)) {
+                points.splice(i, 1); // i - идентификатор, 1 - количество
+                score++; // Увеличиваем счет
+                speed+= 0.01; // увеличиваем скорость
+            }
 
-        // Проверка на столкновение подарка с сантой
+        });
 
-//>>>>>>> master
-        if (el.isIntersect(djostik)) {
-            pjs.vector.points.splice(i, 1); // i - идентификатор, 1 - количество
-            score++; // Увеличиваем счет
-            speed+= 0.01; // увеличиваем скорость
+        if (pjs.keyControl.initKeyControl().isDown('LEFT')) {
+            // Двигаем влево
+            if (djostik.x >= 0)
+                djostik.x -= 5 * dt;
         }
 
-    });
+        if (pjs.keyControl.initKeyControl().isDown('RIGHT')) {
+          // Двигаем влево
+            if (djostik.x+djostik.w < width)
+                djostik.x += 5 * dt;
+        }
 
-    if (pjs.keyControl.initKeyControl().isDown('LEFT')) {
-        // Двигаем влево
-        if (djostik.x >= 0)
-            djostik.x -= 5 * dt;
-    }
-
-    if (pjs.keyControl.initKeyControl().isDown('RIGHT')) {
-      // Двигаем влево
-        if (djostik.x+djostik.w < width)
-            djostik.x += 5 * dt;
-    }
-
-    // Отрисуем счет
-    pjs.brush.drawText({
-        x : 10, y : 10,
-        text : 'Счет: ' + score,
-        size : 20 * r,
-        color : '#FFFFFF',
-        strokeColor : 'black',
-        strokeWidth : 2,
-        style : 'bold',
-        font : 'Arial'
-    });
-
-  };
-
+        // Отрисуем счет
+        pjs.brush.drawText({
+            x : 10, y : 10,
+            text : 'Счет: ' + score,
+            size : 20 * r,
+            color : '#FFFFFF',
+            strokeColor : 'black',
+            strokeWidth : 2,
+            style : 'bold',
+            font : 'Arial'
+        });
+    };
+    /**
+    * Функция новой игры
+    *
+    * Функция, которая при новой игры обновляет счет
+    */
     this.entry = function () { // [optional]
         // При входе в игру будем очищать поинты и удалять счет
-        pjs.OOP.clearArr(pjs.vector.points);
+        pjs.OOP.clearArr(points);
         score = 0;
     };
 
